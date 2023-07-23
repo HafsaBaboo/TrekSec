a3e98705d0f95839a956460bb13dab5e6d4390b7
HafsaBaboo <hafsa.hamzaoui@studenti.unitn.it>
2023-07-23 22:02:13 +0200

    admin log in working

--- static/loginAdmin.js
+++ static/loginAdmin.js
@@ -1,74 +1,72 @@
 var loggedAdmin = {};
-
+var data = '';
 
 async function buttonAd() {
   var admin_email = document.getElementById("email").value;
   var admin_password = document.getElementById("password").value;
 
-  try {
-    const response = await fetch('../api/v1/authenticationsAdmin', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ admin_email: admin_email, admin_password: admin_password }),
-    });
+  fetch('../api/v1/authenticationsAdmin',{
 
-    const data = await response.json();
+    method:'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({admin_email:admin_email, admin_password:admin_password}),
+  
+  })
+  .then((resp) => {
+    data = resp.json();
+    return data;
+  })
 
+  .then(function(data){
     loggedAdmin.token = data.token;
-    loggedAdmin.email = data.admin_email;
+    loggedAdmin.admin_email = data.admin_email;
     loggedAdmin.id = data.id;
     loggedAdmin.self = data.self;
-    loggedAdmin.type = data.admin_type;
-
-    console.log(data.admin_type);
-    console.log(data.admin_email);
-
-
-    Enter();
-
-    console.log(loggedAdmin);
-
-  } catch (error) {
-    console.error(error);
+    loggedAdmin.admin_type = data.admin_type;
+  
+    console.log(loggedAdmin.admin_type);
+    console.log(loggedAdmin.admin_email);
+    console.log(data.success);
+    
+    sessionStorage.setItem('loggedAdmin', loggedAdmin.id); // Salvataggio dei dati nel session storage
+    sessionStorage.setItem('loggedAdminToken', loggedAdmin.token); // Salvataggio dei dati nel session storage
+    sessionStorage.setItem('loggedAdminType', loggedAdmin.type); // Salvataggio dei dati nel session storage
+    Enter(data.success, loggedAdmin.admin_type);
+  
+  })
+  .catch(error => console.error(error));
   }
-}
 
-async function Enter(){
 
-  console.log("prima del controllo");
+async function Enter(dato, type){
+  console.log(type);  
+  console.log(dato);  
 
-  if(loggedAdmin.token && loggedAdmin.email && loggedAdmin.id && loggedAdmin.self){
-        console.log("nel controllo");
-        console.log(loggedAdmin.type);
-
-    if(loggedAdmin.type == "moderator"){
-        
-        console.log("passato Mod");
-        window.location.href = "Moderator.html";
+  if(dato == true){
+    if(type == "moderator"){
+          
+      console.log("passato Mod");
+      window.location.href = "Moderator.html";
 
     }else{
-        if(loggedAdmin.type == "callCenter"){
-            console.log("call");
+      if(type == "callCenter"){
 
-            window.location.href = "CallCenter.html";
-
-        }else{
-
-            if(loggedAdmin.type == "tecnicalSupport"){
-            console.log("tecnical");
-
-            window.location.href = "tecnicalSupport.html";
-            }
-
+        console.log("call");
+        window.location.href = "CallCenter.html";
+        
+      }else{
+        if(type == "tecnicalSupport"){
+          
+          console.log("tecnical");
+          window.location.href = "tecnicalSupport.html";
         }
-
+      }
     }
   }else{
 
     console.log("no giusto");
-
     window.location.href ="loginAdmin.html";
-  }
 
+  }
 }
 
